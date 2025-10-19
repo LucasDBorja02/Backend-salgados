d492182261a4d6db7534016d81e0748a432ae42c
Lucas Daniel de Borja <lucasborja125@gmail.com>
2025-10-19 18:33:22 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,16 @@
-import pkg from 'pg';
-import dotenv from 'dotenv';
-dotenv.config();
-
+import pkg from "pg";
 const { Pool } = pkg;
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false },
 });
+
+// Teste de conexão (só loga se der certo)
+pool
+  .connect()
+  .then(() => console.log("✅ Conectado ao PostgreSQL com sucesso!"))
+  .catch((err) => console.error("❌ Erro ao conectar ao banco:", err.message));
