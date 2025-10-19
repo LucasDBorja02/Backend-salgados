ba75f4987a94bf78a50572e42aa3c42d65b314a7
Lucas Daniel de Borja <lucasborja125@gmail.com>
2025-10-19 17:43:36 -0300

    Refactor server.js and update test route message

--- api/server.js
+++ api/server.js
@@ -1,24 +1,17 @@
 import express from "express";
 import cors from "cors";
 import usuariosRouter from "./router/usuarios.js";
-import produtosRouter from "./router/produtos.js";
-import pedidosRouter from "./router/pedidos.js";
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
 // Rotas
 app.use("/usuarios", usuariosRouter);
-app.use("/produtos", produtosRouter);
-app.use("/pedidos", pedidosRouter);
 
-// Rota de teste
 app.get("/", (req, res) => {
-  res.send("API Salgados da Tia Cida está funcionando!");
+  res.send("API Salgados da Tia Cida está online!");
 });
 
-// ⚠️ Não use app.listen() no Vercel
 export default app;
